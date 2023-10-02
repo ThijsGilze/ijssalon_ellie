c760c2c849dcff9f1d11352d3e4581f880689bd0
thijs <thijs@wait.nu>
2023-10-02 16:37:40 +0200

    opdracht les 7

---
+++ tijdelijk.py
@@ -0,0 +1,42 @@
+#opageve 2.
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5,
+}
+
+#opgave 3
+aanbieding = prijzen["vanille"] * 0.8
+
+#opgave 4
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+#Ik kom niet op 2.40 uit omdat ik niet de value van aardbei, maar vanille pak.
+#print (reclame_tekst)
+
+#opgave 5
+reclame_tekst2 = f"Vandaag in de aanbieding:vanille-ijs, 1 liter - slechts € {aanbieding}"
+#print (reclame_tekst2 [:62])
+
+#opgave 6
+reclame_tekst3 = f"Vandaag in de aanbieding:vanille-ijs, 1 liter - slechts € {aanbieding}"
+#print (reclame_tekst3.upper())
+
+#opgave 7
+reclame_tekst4= f"Vandaag in de aanbieding:vanille-ijs, 1 liter - slechts € {aanbieding}"
+#print (reclame_tekst4.split())
+
+#opgave 8
+#el= reclame_tekst4
+#for el in el:
+#    print (el)
+
+#opgave 9
+#ik heb geen hoofdletters bij opgave 8?Wanneer ik dat wel zou hebben, geef ik het printmethod. 
+#    print (el.lower())
+
+#opgave 10
+(reclame_tekst4.split())
+print (len ("reclame_tekst4"))
+
+
+
